ac2ab8e0416a2dd90f27b9a765b7d8069a538c59
atte8 <swiftchain8@gmail.com>
2025-01-31 17:29:07 +0500

    add kaggle competition model

--- models/models.py
+++ models/models.py
@@ -363,3 +363,128 @@ class Averin_Network(nn.Module):
         x = self.tail(x)
         x = self.classifier(x)
         return x
+
+
+class RuzmetovNetwork(nn.Module):
+    def __init__(self, num_classes):
+
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+
+        self.layer1_conv1 = nn.Conv2d(64, 64, kernel_size=1, bias=False)
+        self.layer1_bn1 = nn.BatchNorm2d(64)
+        self.layer1_conv2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.layer1_bn2 = nn.BatchNorm2d(64)
+        self.layer1_conv3 = nn.Conv2d(64, 256, kernel_size=1, bias=False)
+        self.layer1_bn3 = nn.BatchNorm2d(256)
+        self.layer1_downsample = nn.Sequential(
+            nn.Conv2d(64, 256, kernel_size=1, stride=1, bias=False),
+            nn.BatchNorm2d(256)
+        )
+
+        self.layer2_conv1 = nn.Conv2d(256, 128, kernel_size=1, bias=False)
+        self.layer2_bn1 = nn.BatchNorm2d(128)
+        self.layer2_conv2 = nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1, bias=False)
+        self.layer2_bn2 = nn.BatchNorm2d(128)
+        self.layer2_conv3 = nn.Conv2d(128, 512, kernel_size=1, bias=False)
+        self.layer2_bn3 = nn.BatchNorm2d(512)
+        self.layer2_downsample = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=1, stride=2, bias=False),
+            nn.BatchNorm2d(512)
+        )
+
+        self.layer3_conv1 = nn.Conv2d(512, 256, kernel_size=1, bias=False)
+        self.layer3_bn1 = nn.BatchNorm2d(256)
+        self.layer3_conv2 = nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1, bias=False)
+        self.layer3_bn2 = nn.BatchNorm2d(256)
+        self.layer3_conv3 = nn.Conv2d(256, 1024, kernel_size=1, bias=False)
+        self.layer3_bn3 = nn.BatchNorm2d(1024)
+        self.layer3_downsample = nn.Sequential(
+            nn.Conv2d(512, 1024, kernel_size=1, stride=2, bias=False),
+            nn.BatchNorm2d(1024)
+        )
+
+        # Layer 4
+        self.layer4_conv1 = nn.Conv2d(1024, 512, kernel_size=1, bias=False)
+        self.layer4_bn1 = nn.BatchNorm2d(512)
+        self.layer4_conv2 = nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=1, bias=False)
+        self.layer4_bn2 = nn.BatchNorm2d(512)
+        self.layer4_conv3 = nn.Conv2d(512, 2048, kernel_size=1, bias=False)
+        self.layer4_bn3 = nn.BatchNorm2d(2048)
+        self.layer4_downsample = nn.Sequential(
+            nn.Conv2d(1024, 2048, kernel_size=1, stride=2, bias=False),
+            nn.BatchNorm2d(2048)
+        )
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.dropout = nn.Dropout(p=0.5)
+        self.fc = nn.Linear(2048, num_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        identity = x
+        out = self.layer1_conv1(x)
+        out = self.layer1_bn1(out)
+        out = self.relu(out)
+        out = self.layer1_conv2(out)
+        out = self.layer1_bn2(out)
+        out = self.relu(out)
+        out = self.layer1_conv3(out)
+        out = self.layer1_bn3(out)
+        identity = self.layer1_downsample(identity)
+        out += identity
+        x = self.relu(out)
+
+        identity = x
+        out = self.layer2_conv1(x)
+        out = self.layer2_bn1(out)
+        out = self.relu(out)
+        out = self.layer2_conv2(out)
+        out = self.layer2_bn2(out)
+        out = self.relu(out)
+        out = self.layer2_conv3(out)
+        out = self.layer2_bn3(out)
+        identity = self.layer2_downsample(identity)
+        out += identity
+        x = self.relu(out)
+
+        identity = x
+        out = self.layer3_conv1(x)
+        out = self.layer3_bn1(out)
+        out = self.relu(out)
+        out = self.layer3_conv2(out)
+        out = self.layer3_bn2(out)
+        out = self.relu(out)
+        out = self.layer3_conv3(out)
+        out = self.layer3_bn3(out)
+        identity = self.layer3_downsample(identity)
+        out += identity
+        x = self.relu(out)
+
+        identity = x
+        out = self.layer4_conv1(x)
+        out = self.layer4_bn1(out)
+        out = self.relu(out)
+        out = self.layer4_conv2(out)
+        out = self.layer4_bn2(out)
+        out = self.relu(out)
+        out = self.layer4_conv3(out)
+        out = self.layer4_bn3(out)
+        identity = self.layer4_downsample(identity)
+        out += identity
+        x = self.relu(out)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.dropout(x)
+        x = self.fc(x)
+        return x
